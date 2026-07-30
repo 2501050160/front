043c66d14f17f8f934de1eccfe3ea8ac49f4507a
DHANUSHKUMMARI <dhanushkummari781@gmail.com>
2026-07-31 01:57:22 +0530

    style: render stats cards 2 per row on mobile

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -1884,7 +1884,7 @@ function AdminDashboard() {
                             </div>
                         </motion.section>
 
-                        <section className="mt-6 grid gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6">
+                        <section className="mt-6 grid gap-4 grid-cols-2 lg:grid-cols-3 xl:grid-cols-6">
                             {statCards.map(([label, value, background], index) => (
                                 <motion.div
                                     key={label}
